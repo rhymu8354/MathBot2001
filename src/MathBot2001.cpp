3c12e6393747fd3a2c445083c29b1846f06cafc1
Richard Walters <rwalters@digitalstirling.com>
2018-10-08 20:38:58 -0700

    Refactoring
    Move MathBot2001 to its own module, as a first-class class

---
+++ src/MathBot2001.cpp
@@ -0,0 +1,181 @@
+/**
+ * @file MathBot2001.cpp
+ *
+ * This module contains the implementation of the MathBot2001 class.
+ *
+ * © 2018 by Richard Walters
+ */
+
+#include "MathBot2001.hpp"
+#include "TimeKeeper.hpp"
+
+#include <condition_variable>
+#include <mutex>
+#include <string>
+#include <SystemAbstractions/DiagnosticsSender.hpp>
+#include <SystemAbstractions/File.hpp>
+#include <SystemAbstractions/StringExtensions.hpp>
+#include <Twitch/Messaging.hpp>
+#include <TwitchNetworkTransport/Connection.hpp>
+
+/**
+ * This contains the private properties of a MathBot2001 class instance.
+ */
+struct MathBot2001::Impl
+    : public Twitch::Messaging::User
+{
+    // Properties
+
+    /**
+     * This is a helper object used to generate and publish
+     * diagnostic messages.
+     */
+    SystemAbstractions::DiagnosticsSender diagnosticsSender;
+
+    /**
+     * This is the OAuth token to use in authenticating with Twitch.
+     */
+    std::string token;
+
+    /**
+     * This is used to connect to Twitch chat and exchange messages
+     * with it.
+     */
+    Twitch::Messaging tmi;
+
+    /**
+     * This is used to synchronize access to the object.
+     */
+    std::mutex mutex;
+
+    /**
+     * This is used to signal when any condition for which the main thread
+     * may be waiting has occurred.
+     */
+    std::condition_variable mainThreadEvent;
+
+    /**
+     * This flag is set when the Twitch messaging interface indicates
+     * that the bot has been logged out of Twitch.
+     */
+    bool loggedOut = false;
+
+    // Methods
+
+    /**
+     * This is the default constructor.
+     */
+    Impl()
+        : diagnosticsSender("MathBot2001")
+    {
+    }
+
+    // Twitch::Messaging::User
+
+    virtual void LogIn() override {
+        diagnosticsSender.SendDiagnosticInformationString(1, "Logged in.");
+        tmi.Join("rhymu8354");
+    }
+
+    virtual void LogOut() override {
+        if (loggedOut) {
+            return;
+        }
+        diagnosticsSender.SendDiagnosticInformationString(1, "Logged out.");
+        std::lock_guard< decltype(mutex) > lock(mutex);
+        loggedOut = true;
+        mainThreadEvent.notify_one();
+    }
+
+    virtual void Join(
+        const std::string& channel,
+        const std::string& user
+    ) override {
+        if (user == "mathbot2001") {
+            tmi.SendMessage(channel, "Hello, I'm a bot!");
+        }
+    }
+
+    virtual void Message(
+        Twitch::Messaging::MessageInfo&& messageInfo
+    ) override {
+    }
+
+};
+
+MathBot2001::~MathBot2001() noexcept = default;
+
+MathBot2001::MathBot2001()
+    : impl_(new Impl())
+{
+}
+
+void MathBot2001::Configure(
+    SystemAbstractions::DiagnosticsSender::DiagnosticMessageDelegate diagnosticMessageDelegate
+) {
+    impl_->diagnosticsSender.SubscribeToDiagnostics(diagnosticMessageDelegate, 0);
+    impl_->tmi.SubscribeToDiagnostics(impl_->diagnosticsSender.Chain(), 0);
+    impl_->tmi.SetConnectionFactory(
+        [diagnosticMessageDelegate]() -> std::shared_ptr< Twitch::Connection > {
+            auto connection = std::make_shared< TwitchNetworkTransport::Connection >();
+            connection->SubscribeToDiagnostics(diagnosticMessageDelegate, 0);
+            SystemAbstractions::File caCertsFile(
+                SystemAbstractions::File::GetExeParentDirectory()
+                + "/cert.pem"
+            );
+            if (!caCertsFile.Open()) {
+                diagnosticMessageDelegate(
+                    "MathBot2001",
+                    SystemAbstractions::DiagnosticsSender::Levels::ERROR,
+                    SystemAbstractions::sprintf(
+                        "unable to open root CA certificates file '%s'",
+                        caCertsFile.GetPath().c_str()
+                    )
+                );
+                return nullptr;
+            }
+            std::vector< uint8_t > caCertsBuffer(caCertsFile.GetSize());
+            if (caCertsFile.Read(caCertsBuffer) != caCertsBuffer.size()) {
+                diagnosticMessageDelegate(
+                    "MathBot2001",
+                    SystemAbstractions::DiagnosticsSender::Levels::ERROR,
+                    "unable to read root CA certificates file"
+                );
+                return nullptr;
+            }
+            const std::string caCerts(
+                (const char*)caCertsBuffer.data(),
+                caCertsBuffer.size()
+            );
+            connection->SetCaCerts(caCerts);
+            return connection;
+        }
+    );
+    const auto timeKeeper = std::make_shared< TimeKeeper >();
+    impl_->tmi.SetTimeKeeper(std::make_shared< TimeKeeper >());
+    impl_->tmi.SetUser(
+        std::shared_ptr< Twitch::Messaging::User >(
+            impl_.get(),
+            [](Twitch::Messaging::User*){}
+        )
+    );
+    impl_->diagnosticsSender.SendDiagnosticInformationString(3, "Configured.");
+}
+
+void MathBot2001::InitiateLogIn(const std::string& token) {
+    impl_->tmi.LogIn("MathBot2001", token);
+}
+
+void MathBot2001::InitiateLogOut() {
+    impl_->diagnosticsSender.SendDiagnosticInformationString(3, "Exiting...");
+    impl_->tmi.LogOut("Bye! BibleThump");
+}
+
+bool MathBot2001::AwaitLogOut() {
+    std::unique_lock< decltype(impl_->mutex) > lock(impl_->mutex);
+    return impl_->mainThreadEvent.wait_for(
+        lock,
+        std::chrono::milliseconds(250),
+        [this]{ return impl_->loggedOut; }
+    );
+}
---
+++ src/MathBot2001.hpp
@@ -0,0 +1,84 @@
+#ifndef MATH_BOT_2001_HPP
+#define MATH_BOT_2001_HPP
+
+/**
+ * @file MathBot2001.hpp
+ *
+ * This module declares the MathBot2001 implementation.
+ *
+ * © 2018 by Richard Walters
+ */
+
+#include <Twitch/Messaging.hpp>
+#include <memory>
+
+/**
+ * This represents the chat bot itself.  It handles any callbacks
+ * received from the Twitch messaging interface.
+ */
+class MathBot2001 {
+    // Lifecycle Methods
+public:
+    ~MathBot2001() noexcept;
+    MathBot2001(const MathBot2001&) = delete;
+    MathBot2001(MathBot2001&&) noexcept = delete;
+    MathBot2001& operator=(const MathBot2001&) = delete;
+    MathBot2001& operator=(MathBot2001&&) noexcept = delete;
+
+    // Public Methods
+public:
+    /**
+     * This is the constructor of the class.
+     */
+    MathBot2001();
+
+    /**
+     * This method sets up the bot to interact with the app and with
+     * Twitch chat.
+     *
+     * @param[in] diagnosticMessageDelegate
+     *     This is the function to call to publish any diagnostic messages.
+     */
+    void Configure(
+        SystemAbstractions::DiagnosticsSender::DiagnosticMessageDelegate diagnosticMessageDelegate
+    );
+
+    /**
+     * This method is called to initiate logging into Twitch chat.
+     *
+     * @param[in] token
+     *     This is the OAuth token to use in authenticating with Twitch.
+     */
+    void InitiateLogIn(const std::string& token);
+
+    /**
+     * This method is called to initiate logging out of Twitch chat.
+     */
+    void InitiateLogOut();
+
+    /**
+     * This method waits up to a quarter second for the bot to be
+     * logged out of Twitch.
+     *
+     * @return
+     *     An indication of whether or not the bot has been logged
+     *     out of Twitch is returned.
+     */
+    bool AwaitLogOut();
+
+    // Private properties
+private:
+    /**
+     * This is the type of structure that contains the private
+     * properties of the instance.  It is defined in the implementation
+     * and declared here to ensure that it is scoped inside the class.
+     */
+    struct Impl;
+
+    /**
+     * This contains the private properties of the instance.
+     */
+    std::unique_ptr< Impl > impl_;
+};
+
+#endif /* MATH_BOT_2001_HPP */
